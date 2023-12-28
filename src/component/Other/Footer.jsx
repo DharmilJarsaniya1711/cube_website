61f86dd23556331cd6793331948cdd9b6c7e61c7
Dharmil Patel <dharmil.zestbrains@gmail.com>
2023-12-28 22:37:46 +0530

    first commit

---
+++ src/component/Other/Footer.jsx
@@ -0,0 +1,166 @@
+import React, { Fragment } from "react";
+import { NavLink } from "react-router-dom";
+
+const Footer = (props) => {
+  return (
+    <Fragment>
+      <section className="bg__black">
+        <div className="container">
+          <div className="row justify-content-between">
+            <div className="mb-4 col-md-3">
+              <h4 class="yeseva color__white normal font__size--24 text__24-1024">
+                Cube<span className="color__green"> Studio</span>
+              </h4>
+              <p className="medium font__size--12 text__12-1024 color__white">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. A amet
+                vitae vel id id porttitor ut in. Integer pretium{" "}
+              </p>
+            </div>
+            <div className="mb-4 mb-lg-0 col-6 col-md offset-md-1">
+              <h5 className="medium font__size--12 text__12-1024 color__white mb-3">
+                Quicklink 1
+              </h5>
+              <ul className="list__footer-menu">
+                <li>
+                <NavLink
+                    to="/"
+                    className="color__white medium font__size--12 text__12-1024 opacity__5 hover"
+                  >
+                    Home
+                  </NavLink>
+                </li>
+                <li>
+                <NavLink
+                    to="/about"
+                    className="color__white medium font__size--12 text__12-1024 opacity__5 hover"
+                  >
+                    About Us
+                  </NavLink>
+                </li>
+                <li>
+                <NavLink
+                    to="/portfolio"
+                    className="color__white medium font__size--12 text__12-1024 opacity__5 hover"
+                  >
+                    Portfolio
+                  </NavLink>
+                </li>
+                <li>
+                <NavLink
+                    to="/service"
+                    className="color__white medium font__size--12 text__12-1024 opacity__5 hover"
+                  >
+                    Service
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+            <div className="mb-4 mb-lg-0 col-6 col-md">
+              <h5 className="medium font__size--12 text__12-1024 color__white mb-3">
+                Quicklink 2
+              </h5>
+              <ul className="list__footer-menu">
+                <li>
+                  <NavLink
+                    to="/price"
+                    className="color__white medium font__size--12 text__12-1024 opacity__5 hover"
+                  >
+                    Pricing
+                  </NavLink>
+                </li>
+                <li>
+                <NavLink
+                    to="/blog"
+                    className="color__white medium font__size--12 text__12-1024 opacity__5 hover"
+                  >
+                    Blog
+                  </NavLink>
+                </li>
+                <li>
+                  <a
+                    href="#!"
+                    className="color__white medium font__size--12 text__12-1024 opacity__5 hover"
+                  >
+                    Single Blog
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href="#!"
+                    className="color__white medium font__size--12 text__12-1024 opacity__5 hover"
+                  >
+                    404 Error
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="mb-4 mb-lg-0 col-12 col-md-6 col-lg-3">
+              <div className="mb-4">
+                <h5 className="medium font__size--12 text__12-1024 color__white mb-3">
+                  Join Our Newsletter{" "}
+                </h5>
+
+                <div className="wrapper__subscribe d-flex align-items-center">
+                  <input
+                    type="text"
+                    className="form-control color__white medium font__size--12 text__12-1024"
+                    placeholder="Your Email Address"
+                  />
+                  <button className="medium font__size--12 text__12-1024 btn btn__green shadow color__white shadow ml-3">
+                    Send
+                  </button>
+                </div>
+              </div>
+
+              <h5 className="medium font__size--12 text__12-1024 color__white mb-3">
+                Follow Us
+              </h5>
+
+              <ul className="list__sosmed">
+                <li>
+                  <a href="#!">
+                    <img src="./../images/sos (1).png" alt="" />
+                  </a>
+                </li>
+                <li>
+                  <a href="#!">
+                    <img src="./../images/sos (2).png" alt="" />
+                  </a>
+                </li>
+                <li>
+                  <a href="#!">
+                    <img src="./../images/sos (3).png" alt="" />
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-6 col-lg">
+              <h5 className="medium font__size--12 text__12-1024 color__white mb-3">
+                Download Our App
+              </h5>
+              <ul className="list__footer-menu">
+                <li>
+                  <a href="#!">
+                    <img src="./../images/Google Play.png" alt="" />
+                  </a>
+                </li>
+                <li>
+                  <a href="#!">
+                    <img src="./../images/App Store.png" alt="" />
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className="bg__black-2 py-4">
+        <div className="container color__gray-2 text-center font__size--12 text__12-1024 medium">
+          Design and develop by Devoq Design{" "}
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default Footer;
---
+++ src/container/homepage/Error.jsx
@@ -0,0 +1,49 @@
+import React, { Fragment } from "react";
+import Footer from "../../component/Other/Footer";
+import Navbar from "../../component/Navbar/Navbar";
+import { NavLink } from "react-router-dom";
+const Error = (props) => {
+  return (
+    <Fragment>
+      <div className="overflow-hidden position-relative">
+        <div className="position-relative">
+          <img
+            src="./../images/new/dsad.png"
+            className="images__bg-head"
+            alt=""
+          />
+          <div className="position-relative z-2">
+            <Navbar />
+            <section className="height__100">
+              <div className="container">
+                <div className="text-center">
+                  <h2 className="font__size--64 text__65-1024 text__65-sm text__65-xxs yeseva">
+                    404 <br />
+                    Page Not Found
+                  </h2>
+                  <p className="medium font__size--14 text__14-1024 lh-2 color__gray-1 mb-3">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. A
+                    amet vitae vel id id porttitor ut in. <br /> Integer pretium
+                    egestas nisi, nunc, purus sit vitae nisi, etiam.{" "}
+                  </p>
+                  <NavLink
+                    to="/"
+                  >
+                  <button className="btn btn__green medium font__size--14 text__14-1024 color__white shadow">
+
+                    Back to Homepage
+                  </button>
+                  </NavLink>
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+
+        <Footer />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Error;
